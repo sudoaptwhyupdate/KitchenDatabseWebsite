b9361798bb3f38ce71982d2e01ae32fe25b8ef66
sudoaptwhyupdate <71827167+sudoaptwhyupdate@users.noreply.github.com>
2022-02-21 09:20:48 -0500

    Add files via upload

---
+++ website/models.py
@@ -0,0 +1,26 @@
+import email
+from enum import unique
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+"""
+  Item Object, handles name of of item, the date it was last recorded, and the 
+  quantity of that item
+  
+  One User many items
+"""
+class Item(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(10000))
+  date = db.Column(db.DateTime(timezone=True), default=func.now())
+  quantity = db.Column(db.String(1000))
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  
+# User object, handles email, password, and relationship to items that the person has
+class User(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(150), unique=True)
+  password = db.Column(db.String(150))
+  user_name = db.Column(db.String(150))
+  items = db.relationship('Item')
