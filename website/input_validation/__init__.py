23cdf88027d47e31ed747ac334f554052164596f
Brendan Fineran <bfineran727@gmail.com>
2022-03-30 10:10:08 -0400

    finished error message function
    function is declared privately so that user does not touch it and cause
    errors

--- website/input_validation/__init__.py
+++ website/input_validation/__init__.py
@@ -1,6 +1,9 @@
 # input validation module to figure out if all things inputted by the user
 # are fine
 
+from flask import Flask
+
+
 class ValidateInput:
   
   # This is a simple input validation class
@@ -18,23 +21,59 @@ class ValidateInput:
       name_min=0,
       name_max=0,
   ):
-    
-
-      # please forgive me, the way this code look pains me too
-      # but the only other way was unreadable to my eyes 
-      # and this is clear enough
-      # if there is a better way to write it please fix it
-      #
-      # number_of_attempts_fixing_it = 4
       
       self.data_type = data_type
       self.name = name
       self.name_min = name_min
       self.name_max = name_max
 
+  def __error_message_generator(self, bad_data, length=0, minimum=0, exceeds=0, type=""):
+    
+    if length == 0 and exceeds == 0:
+      type_problem = True
+    else: 
+      type_problem = False
+    
+    bounds_error_message = f"""
+    
+    There was a problem detected in the input given:
+    
+      {bad_data}
+    
+    This input either exceeds or does not meet the minimum length requested
+    by developer.
+    
+    Given Data: {bad_data}
+    Data Length: {length}
+    Length Requested: needs to be less than {exceeds} but more than {minimum} 
+    
+    """
+    
+    type_error_message = f"""
+    
+    There was a problem detected in the input given 
+
+      {bad_data}
+    
+    This input does not meet the requirements (data_type) requested
+    by the developer.
+    
+    Given Data: {bad_data}
+    Data Type Given: {type(bad_data)}
+    Data Type Requested: {type}
+    
+    """
+    
+    if type_problem == False:
+      return bounds_error_message
+    if type_problem == True:
+      return type_error_message
+      
+  
   @classmethod
   def _len_check(self, name, min, max):
       if len(name) >= max or name <= min:
+        self.__error_message_generator(name, len(name), min, max)
         return "error"
       else:
         return "success"
